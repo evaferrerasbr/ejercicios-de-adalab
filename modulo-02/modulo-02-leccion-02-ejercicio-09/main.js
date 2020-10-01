abb235e8c13ec0e0ad2466201d0341c216f8d59b
evaferrerasbr <evaferrerasbr@gmail.com>
2020-10-01 19:29:07 +0200

    Initial commit characters

---
+++ modulo-02/modulo-02-leccion-02-ejercicio-09/main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function characters(name) {
+  console.log(
+    `El nombre de mi compañera es ${name} y tiene ${
+      name.length - 2
+    } caracteres.`
+  );
+}
+
+const myName = "Eva Ferreras Bracho";
+
+characters(myName);
