c914863580398eaae8f1505c6824525bf0ec26af
evaferrerasbr <evaferrerasbr@gmail.com>
2020-10-02 11:18:28 +0200

    Update pair programming

--- modulo-02/modulo-02-leccion-02-ejercicio-09/main.js
+++ modulo-02/modulo-02-leccion-02-ejercicio-09/main.js
@@ -1,13 +1,24 @@
 "use strict";
 
-function characters(name) {
-  console.log(
-    `El nombre de mi compañera es ${name} y tiene ${
-      name.length - 2
-    } caracteres.`
-  );
-}
+// function characters(name) {
+//   console.log(
+//     `El nombre de mi compañera es ${name} y tiene ${
+//       name.length - 2
+//     } caracteres.`
+//   );
+// }
 
-const myName = "Eva Ferreras Bracho";
+// const myName = "Eva Ferreras Bracho";
 
-characters(myName);
+// characters(myName);
+
+const name = "Eva";
+const firstSurname = "Ferreras";
+const secondSurname = "Bracho";
+console.log(
+  `El nombre de mi compañera es ${
+    name + " " + firstSurname + " " + secondSurname
+  } y tiene ${
+    name.length + firstSurname.length + secondSurname.length
+  } caracteres.`
+);
--- modulo-02/modulo-02-leccion-02-ejercicio-10/main.js
+++ modulo-02/modulo-02-leccion-02-ejercicio-10/main.js
@@ -2,5 +2,6 @@
 
 const userAge = document.querySelector(".user-age");
 console.log(userAge.innerHTML);
-const hoursLived = parseInt(userAge.innerHTML) * 8760;
+const yearHours = 8760;
+const hoursLived = parseInt(userAge.innerHTML) * yearHours;
 console.log(hoursLived);
