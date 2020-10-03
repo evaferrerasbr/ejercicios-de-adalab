cafbfdc5c0d0e7153798a20316160eebf6e0323d
evaferrerasbr <evaferrerasbr@gmail.com>
2020-10-03 19:27:47 +0200

    Add function (bisiestos)

--- modulo-02/modulo-02-leccion-03-ejercicio-08/main.js
+++ modulo-02/modulo-02-leccion-03-ejercicio-08/main.js
@@ -1,13 +1,17 @@
 "use strict";
 
-let year = 2020;
-
-if (year % 4 === 0) {
-  console.log("¡Estás en un año bisiesto!");
-} else if ((year + 1) % 4 === 0) {
-  console.log(`El siguiente año bisiesto será ${year + 1}`);
-} else if ((year + 2) % 4 === 0) {
-  console.log(`El siguiente año bisiesto será ${year + 2}`);
-} else if ((year + 3) % 4 === 0) {
-  console.log(`El siguiente año bisiesto será ${year + 3}`);
+function year(year) {
+  if (year % 4 === 0) {
+    console.log("¡Estás en un año bisiesto!");
+  } else if ((year + 1) % 4 === 0) {
+    console.log(`El siguiente año bisiesto será ${year + 1}`);
+  } else if ((year + 2) % 4 === 0) {
+    console.log(`El siguiente año bisiesto será ${year + 2}`);
+  } else if ((year + 3) % 4 === 0) {
+    console.log(`El siguiente año bisiesto será ${year + 3}`);
+  }
 }
+
+year(2020);
+year(2029);
+year(3);
