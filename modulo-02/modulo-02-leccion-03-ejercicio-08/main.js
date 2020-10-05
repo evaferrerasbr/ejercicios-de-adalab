bd2bfce08b8564b2089104af73e85525bed58f81
evaferrerasbr <evaferrerasbr@gmail.com>
2020-10-05 11:21:27 +0200

    Test pair programming

--- modulo-02/modulo-02-leccion-03-ejercicio-08/main.js
+++ modulo-02/modulo-02-leccion-03-ejercicio-08/main.js
@@ -1,17 +1,29 @@
 "use strict";
 
-function year(year) {
-  if (year % 4 === 0) {
-    console.log("¡Estás en un año bisiesto!");
-  } else if ((year + 1) % 4 === 0) {
-    console.log(`El siguiente año bisiesto será ${year + 1}`);
-  } else if ((year + 2) % 4 === 0) {
-    console.log(`El siguiente año bisiesto será ${year + 2}`);
-  } else if ((year + 3) % 4 === 0) {
-    console.log(`El siguiente año bisiesto será ${year + 3}`);
-  }
+const year = 2024;
+
+if (year % 4 === 0) {
+  console.log("¡Estás en un año bisiesto!");
+} else if (year % 4 === 1) {
+  console.log(`El siguiente año bisiesto será ${year + 3}`);
+} else if (year % 4 === 2) {
+  console.log(`El siguiente año bisiesto será ${year + 2}`);
+} else if (year % 4 === 3) {
+  console.log(`El siguiente año bisiesto será ${year + 1}`);
 }
 
-year(2020);
-year(2029);
-year(3);
+// function year(year) {
+//   if (year % 4 === 0) {
+//     console.log("¡Estás en un año bisiesto!");
+//   } else if ((year + 1) % 4 === 0) {
+//     console.log(`El siguiente año bisiesto será ${year + 1}`);
+//   } else if ((year + 2) % 4 === 0) {
+//     console.log(`El siguiente año bisiesto será ${year + 2}`);
+//   } else if ((year + 3) % 4 === 0) {
+//     console.log(`El siguiente año bisiesto será ${year + 3}`);
+//   }
+// }
+
+// year(2020);
+// year(2029);
+// year(3);
