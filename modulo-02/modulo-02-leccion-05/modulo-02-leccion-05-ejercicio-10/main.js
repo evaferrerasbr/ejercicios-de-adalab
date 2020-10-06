dbd61987617fb9b88dc17aa0cbe2cb2840e4518e
evaferrerasbr <evaferrerasbr@gmail.com>
2020-10-06 19:41:29 +0200

    Initial commit (select movie -not finished-)

---
+++ modulo-02/modulo-02-leccion-05/modulo-02-leccion-05-ejercicio-10/main.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const inception = "Inception";
+const theButterFlyEffect = "The butterfly effect";
+const eternalSunshineOfTheSM = "Eternal sunshine of the spotless mind";
+const blueVelvet = "Blue velvet";
+const split = "Split";
+
+const button = document.querySelector(".button");
+const list = document.querySelector(".list");
+
+function addListEl() {
+  list.innerHTML = `<li>${inception}</li><li>${theButterFlyEffect}</li><li>${eternalSunshineOfTheSM}</li><li>${blueVelvet}</li><li>${split}</li>`;
+}
+
+button.addEventListener("click", addListEl);
