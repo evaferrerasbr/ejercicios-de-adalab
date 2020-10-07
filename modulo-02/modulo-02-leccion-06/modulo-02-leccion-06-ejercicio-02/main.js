7c92c58797282935dda5db08d1fa9ab355311c55
evaferrerasbr <evaferrerasbr@gmail.com>
2020-10-07 19:29:23 +0200

    Initial commit (run a marathon)

---
+++ modulo-02/modulo-02-leccion-06/modulo-02-leccion-06-ejercicio-02/main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const adalaber = {
+  name: "Susana",
+  age: 35,
+  job: "periodista",
+};
+
+// adalaber.run = () => "Estoy corriendo";
+// console.log(adalaber.run());
+
+adalaber.runAMarathon = (distance) => `Estoy corriendo ${distance} kilómetros`;
+console.log(adalaber.runAMarathon(7));
