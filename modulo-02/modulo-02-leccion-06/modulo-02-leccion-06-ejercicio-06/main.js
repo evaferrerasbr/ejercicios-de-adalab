ced9d40467517062b5324a87c9b8b2b66c97726d
evaferrerasbr <evaferrerasbr@gmail.com>
2020-10-07 20:34:04 +0200

    Initial commit (pear basket)

---
+++ modulo-02/modulo-02-leccion-06/modulo-02-leccion-06-ejercicio-06/main.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const pearBasket = {
+  min: 0,
+  max: 7,
+  current: 4,
+  initial: 1,
+};
+
+pearBasket.addPear = function () {
+  if (pearBasket.current < 7) {
+    pearBasket.current = pearBasket.current + 1;
+    console.log(pearBasket.current);
+  }
+};
+
+pearBasket.removePear = function () {
+  if (pearBasket.current > 0) {
+    pearBasket.current = pearBasket.current - 1;
+    console.log(pearBasket.current);
+  }
+};
+
+pearBasket.reset = function () {
+  pearBasket.current = 4;
+  console.log(pearBasket.current);
+};
