ccb0b0343a0b4d6c10b9b6a24c886d4b4c94ae91
evaferrerasbr <evaferrerasbr@gmail.com>
2020-10-17 17:19:51 +0200

    Initial commit (character searcher from Star Wars API)

---
+++ modulo-02/modulo-02-leccion-11/modulo-02-leccion-11-ejercicio-02/main.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const btn = document.querySelector(".js-button");
+const list = document.querySelector(".js-list");
+
+function getInfo() {
+  list.innerHTML = "";
+  let search = document.querySelector(".js-input").value;
+  for (let i = 1; i <= 9; i++) {
+    fetch(`https://swapi.dev/api/people/?page=${i}`)
+      .then((response) => response.json())
+      .then((data) => {
+        let results = data.results;
+        for (let j = 0; j < results.length; j++) {
+          let characterName = results[j].name;
+          let characterGender = results[j].gender;
+          if (characterName.includes(search)) {
+            let liElement = `<li>Name: <span class="ligthText">${characterName}</span> Gender: <span class="ligthText">${characterGender}</span></li>`;
+            list.innerHTML += liElement;
+          }
+        }
+      });
+  }
+}
+
+btn.addEventListener("click", getInfo);
