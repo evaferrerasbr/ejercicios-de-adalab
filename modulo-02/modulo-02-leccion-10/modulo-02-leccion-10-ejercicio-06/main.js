cdf942034d549c615df7a2abceb9baa4ab2821d6
evaferrerasbr <evaferrerasbr@gmail.com>
2020-10-18 16:03:13 +0200

    Fix fetch issue by adding an empty global array

--- modulo-02/modulo-02-leccion-10/modulo-02-leccion-10-ejercicio-06/main.js
+++ modulo-02/modulo-02-leccion-10/modulo-02-leccion-10-ejercicio-06/main.js
@@ -1,23 +1,29 @@
 'use strict';
 
+const dogs = [];
+
 function getRandomDog() {
   fetch('https://dog.ceo/api/breeds/list')
     .then((breedsResponse) => breedsResponse.json())
     .then((breedsData) => {
-      const breedsLegth = breedsData.message;
-      const randomMax = breedsLegth.length;
-      console.log(randomMax);
-      return fetch(`https://rand.fun/integer?max=${randomMax}`);
+      const breeds = breedsData.message;
+      for (let i = 0; i < breeds.length; i++) {
+        dogs.push(breeds[i]);
+      }
+      return fetch(`https://api.rand.fun/number/integer?max=${dogs.length}`);
     })
     .then((randomResponse) => randomResponse.json())
     .then((randomData) => {
-      const randomBreed = breedsData[randomData.result];
-      document.querySelector('.js-text').innerHTML = randomBreed;
-      return fetch(`https://dog.ceo/api/breed/${randomBreed}/images/random`);
+      const randomBreed = randomData.result;
+      console.log(randomBreed);
+      document.querySelector('.js-text').innerHTML = dogs[randomBreed];
+      return fetch(
+        `https://dog.ceo/api/breed/${dogs[randomBreed]}/images/random`
+      );
     })
     .then((imageResponse) => imageResponse.json())
     .then((imageData) => {
-      const img = document.querySelector('.js-img');
+      const img = document.querySelector('.js-image');
       img.src = imageData.message;
       img.alt = 'Un perrete';
     });
