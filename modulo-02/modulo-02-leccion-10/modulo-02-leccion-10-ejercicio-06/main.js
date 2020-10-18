56bd2dde56f0d3c84b79e541a006cd0d622aa592
evaferrerasbr <evaferrerasbr@gmail.com>
2020-10-18 14:38:42 +0200

    Initial commit (get a random dog image using rand.fun/integer)

---
+++ modulo-02/modulo-02-leccion-10/modulo-02-leccion-10-ejercicio-06/main.js
@@ -0,0 +1,24 @@
+'use strict';
+
+function getRandomDog() {
+  fetch('https://dog.ceo/api/breeds/list')
+    .then((breedsResponse) => breedsResponse.json())
+    .then((breedsData) => {
+      const breedsLegth = breedsData.message;
+      const randomMax = breedsLegth.length;
+      return fetch(`https://rand.fun/integer?max=${randomMax}`);
+    })
+    .then((randomResponse) => randomResponse.json())
+    .then((randomData) => {
+      const randomBreed = breedsData[randomData.result];
+      document.querySelector('.js-text').innerHTML = randomBreed;
+      return fetch(`https://dog.ceo/api/breed/${randomBreed}/images/random`);
+    })
+    .then((imageResponse) => imageResponse.json())
+    .then((imageData) => {
+      const img = document.querySelector('.js-img');
+      img.src = imageData.message;
+      img.alt = 'Un perrete';
+    });
+}
+document.querySelector('.js-button').addEventListener('click', getRandomDog);
