0e803a73e51b0e5bd4f3db15fc8c00e0b4f7cd5a
evaferrerasbr <evaferrerasbr@gmail.com>
2020-10-18 13:11:41 +0200

    Change saveTheme() for asigning savedTheme to themes

--- modulo-02/modulo-02-leccion-11/modulo-02-leccion-11-ejercicio-05/main.js
+++ modulo-02/modulo-02-leccion-11/modulo-02-leccion-11-ejercicio-05/main.js
@@ -5,20 +5,18 @@ const form = document.querySelector(".js-form");
 
 let themes = {};
 
-changeTheme();
-
 function changeTheme() {
   if (themes.light === true) {
     body.classList.add("theme-1");
-    body.classList.remove("theme-2");
   }
   if (themes.dark === true) {
     body.classList.add("theme-2");
-    body.classList.remove("theme-1");
   }
 }
 
 function selectOption(event) {
+  body.classList.remove("theme-1");
+  body.classList.remove("theme-2");
   if (event.target.value === "theme-1") {
     themes.light = true;
     themes.dark = false;
@@ -29,12 +27,11 @@ function selectOption(event) {
   }
   changeTheme();
   localStorage.setItem("themes", JSON.stringify(themes));
-  return themes;
 }
 
 function saveTheme() {
-  const savedTheme = localStorage.getItem("themes");
-  JSON.parse(savedTheme);
+  const savedTheme = JSON.parse(localStorage.getItem("themes"));
+  themes = savedTheme;
   changeTheme();
 }
 
