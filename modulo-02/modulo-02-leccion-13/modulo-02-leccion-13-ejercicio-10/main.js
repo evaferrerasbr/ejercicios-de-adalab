2157d0bfc772017f3de1162c20c7d10b3f41fa85
evaferrerasbr <evaferrerasbr@gmail.com>
2020-10-20 20:17:05 +0200

    Find the user with the pin 5232)

---
+++ modulo-02/modulo-02-leccion-13/modulo-02-leccion-13-ejercicio-10/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const users = [
+  { name: 'María', isPremium: false, pin: 2389 },
+  { name: 'Lucía', isPremium: true, pin: 2384 },
+  { name: 'Susana', isPremium: true, pin: 2837 },
+  { name: 'Rocío', isPremium: false, pin: 5232 },
+  { name: 'Inmaculada', isPremium: false, pin: 8998 },
+];
+
+const selectedUser = users.find((user) => user.pin === 5232);
+
+console.log(selectedUser);
