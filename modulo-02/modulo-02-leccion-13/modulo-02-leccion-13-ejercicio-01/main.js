ac1f660a806989605e2c806e490823d2d6634f34
evaferrerasbr <evaferrerasbr@gmail.com>
2020-10-20 18:51:15 +0200

    Initial commit (cheated marks)

--- modulo-02/modulo-02-leccion-13/modulo-02-leccion-13-ejercicio-01/main.js
+++ modulo-02/modulo-02-leccion-13/modulo-02-leccion-13-ejercicio-01/main.js
@@ -1,13 +1,6 @@
-"use strict";
+'use strict';
 
-const kiwisPrice = 5;
-const perasPrice = 2;
-const grapesPrice = 4;
-const myKiwisPrice = 2 * kiwisPrice;
-console.log(myKiwisPrice);
-const myPerasPrice = 3 * perasPrice;
-console.log(myPerasPrice);
-const myGrapesPrice = 0.5 * grapesPrice;
-console.log(myGrapesPrice);
-const result = myKiwisPrice + myPerasPrice + myGrapesPrice;
-console.log(result);
+const marks = [5, 4, 6, 7, 9];
+const cheatedMarks = marks.map((mark) => mark + 1);
+
+console.log(cheatedMarks);
