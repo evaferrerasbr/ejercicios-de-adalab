8deea199b1b26a39aca07a36df871d88d101c322
evaferrerasbr <evaferrerasbr@gmail.com>
2020-10-21 18:03:38 +0200

    Add comments to document the issues

--- modulo-02/modulo-02-leccion-11/modulo-02-leccion-11-ejercicio-08/main.js
+++ modulo-02/modulo-02-leccion-11/modulo-02-leccion-11-ejercicio-08/main.js
@@ -9,32 +9,34 @@ let results = undefined;
 
 //esta función es la que se activa cuando la usuaria hace click en Buscar tras meter el término de búsqueda; llama a la función getLocalStorage() y, en caso de que su return sea false, llama a la función getInfo();
 function handler() {
-  console.log('entro');
+  console.log('entro en el handler porque hago clic');
+  search = document.querySelector('.js-input').value;
   list.innerHTML = '';
-  const askLocalStorage = getLocalStorage();
-  if (!askLocalStorage) {
-    getInfo();
+  if (search !== '') {
+    console.log('entro aquí porque la búsqueda no está vacía');
+    const askLocalStorage = getLocalStorage();
+    if (!askLocalStorage) {
+      console.log('entro aquí porque no hay nada en el local storage');
+      getInfo();
+    } else {
+      list.innerHTML = '';
+    }
   }
 }
 
 //si es la primera vez que la usuaria visita la página, esta función hace una petición al servidor con la búsqueda de la usuaria (que guarda en la variable global search), guarda la información .results del json y llama a painData().
 function getInfo() {
-  list.innerHTML = '';
-  search = document.querySelector('.js-input').value;
-  if (search !== '') {
-    console.log('entro en el if porque no estoy vacío');
-    fetch(`https://swapi.dev/api/people/?search=${search}`)
-      .then((response) => response.json())
-      .then((data) => {
-        results = data.results;
-        console.log('estoy al final de getInfo');
-        paintData();
-      });
-  }
+  fetch(`https://swapi.dev/api/people/?search=${search}`)
+    .then((response) => response.json())
+    .then((data) => {
+      results = data.results;
+      paintData();
+    });
 }
 
 //esta función recorre results y por cada resultado que coincida con la búsqueda de la usuaria nos crea un elemento <li> que recoge la propiedad name y la propiedad gender.
 function paintData() {
+  console.log('te pinto los resultados en pantalla');
   for (let j = 0; j < results.length; j++) {
     let liElement = `<li>Name: <span class="ligthText">${results[j].name}</span> Gender: <span class="ligthText">${results[j].gender}</span></li>`;
     list.innerHTML += liElement;
@@ -50,6 +52,7 @@ function paintData() {
 
 //la función local storage busca en el almacenamiento (que, si no es la primera vez que entra, estará lleno) y guarda en la variable global savedSearch lo que hubiésemos guardado.
 function getLocalStorage() {
+  console.log('te miro en el local storage');
   const stringInfo = localStorage.getItem('search');
   savedSearch = JSON.parse(stringInfo);
   if (savedSearch !== null) {
@@ -60,13 +63,18 @@ function getLocalStorage() {
   }
 }
 
+//esta función pregunta si el local storage contiene el término de búsqueda (aunque falta perfilarlo para que no tenga case sensitivity). Falta que me pinte los elementos en pantalla, ya que entra por el if (imprime en pantalla el console.log de dentro) pero no pinta nada en la lista. Cuando está en el debugger sí lo pinta. También falta darle bien una orden alternativa al if para que se vaya a buscar la información al fetch si no está lo que queremos. Por ahora he intentado invocar a getInfo() y no funciona porque me multiplica la información del localstorage.
 function getInfoFromLocal() {
+  console.log('el local storage no está vacío');
   for (let i = 0; i < savedSearch.length; i++) {
-    if (savedSearch[i].name.includes(search)) {
+    let character = savedSearch[i].name;
+    console.log(character);
+    console.log(search);
+    console.log(character.includes(search));
+    if (character.includes(search)) {
+      console.log('entiendo la orden');
       let liElement = `<li>Name: <span class="ligthText">${savedSearch[i].name}</span> Gender: <span class="ligthText">${savedSearch[i].gender}</span></li>`;
       list.innerHTML += liElement;
-    } else {
-      getInfo();
     }
   }
 }
