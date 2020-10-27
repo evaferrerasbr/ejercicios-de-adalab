0301f841f9acf6276847b43fb83fcc28bd222aca
evaferrerasbr <evaferrerasbr@gmail.com>
2020-10-27 17:22:01 +0100

    Add sort for alphabetical order

--- modulo-02/modulo-02-leccion-13/modulo-02-leccion-13-ejercicio-12/main.js
+++ modulo-02/modulo-02-leccion-13/modulo-02-leccion-13-ejercicio-12/main.js
@@ -1,15 +1,23 @@
 'use strict';
 
 const users = [
-  { name: 'María', isPremium: false, pin: 2389 },
-  { name: 'Lucía', isPremium: true, pin: 2384 },
-  { name: 'Susana', isPremium: true, pin: 2837 },
   { name: 'Rocío', isPremium: false, pin: 5232 },
+  { name: 'María', isPremium: false, pin: 2389 },
   { name: 'Inmaculada', isPremium: false, pin: 8998 },
+  { name: 'Susana', isPremium: true, pin: 2837 },
+  { name: 'Lucía', isPremium: true, pin: 2384 },
 ];
 
 users.sort((a, b) => a.pin - b.pin);
+console.log(users);
 
-//falta ordenarlos alfabéticamente.
-
+users.sort((a, b) => {
+  if (a.name > b.name) {
+    return 1;
+  }
+  if (a.name < b.name) {
+    return -1;
+  }
+  return 0;
+});
 console.log(users);
