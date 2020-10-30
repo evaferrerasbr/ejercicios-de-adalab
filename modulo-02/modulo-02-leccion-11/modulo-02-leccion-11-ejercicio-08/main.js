b8914a5bc065f1adce6aef6cc58a1ea1d15c944f
evaferrerasbr <evaferrerasbr@gmail.com>
2020-10-30 18:43:56 +0100

    Fix getInfoFromLocal() to not include a search in local storage more than once and clean comments

--- modulo-02/modulo-02-leccion-11/modulo-02-leccion-11-ejercicio-08/main.js
+++ modulo-02/modulo-02-leccion-11/modulo-02-leccion-11-ejercicio-08/main.js
@@ -6,17 +6,14 @@ const characterInfo = [];
 let savedSearch = [];
 let search = '';
 let results = '';
+let marker = false;
 
-//esta función es la que se activa cuando la usuaria hace click en Buscar tras meter el término de búsqueda; si el término de búsqueda no está vacío llama a la función getLocalStorage() para consultar el local storage. En caso de esté vacío llama a la función getInfo() para buscar con el fetch, y si está lleno llama a getInfoFromLocal();
 function handler() {
-  console.log('entro en el handler porque hago clic');
-  search = document.querySelector('.js-input').value;
-  list.innerHTML = '';
+  marker = false;
+  search = document.querySelector('.js-input').value.toLowerCase();
   if (search !== '') {
-    console.log('entro aquí porque la búsqueda no está vacía');
     const askLocalStorage = getLocalStorage();
     if (!askLocalStorage) {
-      console.log('entro aquí porque no hay nada en el local storage');
       getInfo();
     } else {
       getInfoFromLocal();
@@ -24,7 +21,6 @@ function handler() {
   }
 }
 
-//si es la primera vez que la usuaria visita la página, esta función hace una petición al servidor con la búsqueda de la usuaria (que guarda en la variable global search), guarda la información .results del json y llama a painData().
 function getInfo() {
   fetch(`https://swapi.dev/api/people/?search=${search}`)
     .then((response) => response.json())
@@ -34,9 +30,8 @@ function getInfo() {
     });
 }
 
-//esta función recorre results y por cada resultado que coincida con la búsqueda de la usuaria nos crea un elemento <li> que recoge la propiedad name y la propiedad gender.
 function paintData() {
-  console.log('te pinto los resultados en pantalla');
+  list.innerHTML = '';
   for (let j = 0; j < results.length; j++) {
     let liElement = `<li>Name: <span class="ligthText">${results[j].name}</span> Gender: <span class="ligthText">${results[j].gender}</span></li>`;
     list.innerHTML += liElement;
@@ -50,9 +45,7 @@ function paintData() {
   localStorage.setItem('search', stringData);
 }
 
-//la función local storage busca en el almacenamiento (que, si no es la primera vez que entra, estará lleno) y guarda en la variable global savedSearch lo que hubiésemos guardado.
 function getLocalStorage() {
-  console.log('te miro en el local storage');
   const stringInfo = localStorage.getItem('search');
   savedSearch = JSON.parse(stringInfo);
   if (savedSearch !== null) {
@@ -62,18 +55,15 @@ function getLocalStorage() {
   }
 }
 
-let marker = false;
-
-//esta función pregunta si el local storage contiene el término de búsqueda (aunque falta perfilarlo para que no tenga case sensitivity). Gracias a la variable marker podemos preguntar si ha encontrado el término de búsqueda en los resultados del local storage. En caso de no haberlos encontrado, llamamos a la función que hace la petición al servidor.
 function getInfoFromLocal() {
-  console.log('el local storage no está vacío');
   for (let i = 0; i < savedSearch.length; i++) {
-    let character = savedSearch[i].name;
+    let character = savedSearch[i].name.toLowerCase();
     console.log(character);
     console.log(search);
     console.log(character.includes(search));
     if (character.includes(search)) {
       marker = true;
+      list.innerHTML = '';
       console.log('entiendo la orden');
       let liElement = `<li>Name: <span class="ligthText">${savedSearch[i].name}</span> Gender: <span class="ligthText">${savedSearch[i].gender}</span></li>`;
       list.innerHTML += liElement;
