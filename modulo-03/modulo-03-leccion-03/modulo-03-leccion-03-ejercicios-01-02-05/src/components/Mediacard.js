f2b783826f147b55b60d5c2dd31dc820c98d9155
evaferrerasbr <evaferrerasbr@gmail.com>
2020-11-03 20:36:09 +0100

    Remove useless constant

--- modulo-03/modulo-03-leccion-03/modulo-03-leccion-03-ejercicios-01-02-05/src/components/Mediacard.js
+++ modulo-03/modulo-03-leccion-03/modulo-03-leccion-03-ejercicios-01-02-05/src/components/Mediacard.js
@@ -6,8 +6,6 @@ import logo from '../images/mabelpines.jpg';
 const text =
   'Lorem fistrum no te digo trigo por no llamarte Rodrigor a gramenawer ese que llega de la pradera. De la pradera a wan a gramenawer diodeno ese que llega. Ese hombree fistro al ataquerl pupita jarl al ataquerl. Pecador a gramenawer torpedo tiene musho peligro apetecan ese hombree va usté muy cargadoo. No puedor sexuarl mamaar me cago en tus muelas a peich la caidita. Te voy a borrar el cerito pupita mamaar va usté muy cargadoo papaar papaar.';
 
-const likes = 37;
-
 class Mediacard extends React.Component {
   render() {
     return (
