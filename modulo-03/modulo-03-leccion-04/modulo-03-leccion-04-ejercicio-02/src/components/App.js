bda53979badbb59ab822db6a2232178e2d125a68
evaferrerasbr <evaferrerasbr@gmail.com>
2020-11-04 20:27:47 +0100

    Create a component that uses Props.children (error with React.DOM)

---
+++ modulo-03/modulo-03-leccion-04/modulo-03-leccion-04-ejercicio-02/src/components/App.js
@@ -0,0 +1,12 @@
+import '../stylesheets/App.scss';
+import Popup from './Popup';
+
+function App() {
+  return (
+    <div className="App">
+      <Popup />
+    </div>
+  );
+}
+
+export default App;
