f8b4ee97dd2785240ceac1923e71390bf88c486f
evaferrerasbr <evaferrerasbr@gmail.com>
2020-11-07 21:00:03 +0100

    Change function to add two params instead of one

---
+++ modulo-03/modulo-03-leccion-05/modulo-03-leccion-05-ejercicio-08-09/src/components/App.js
@@ -0,0 +1,12 @@
+import '../stylesheets/App.scss';
+import Form from './Form';
+
+function App() {
+  return (
+    <div className="App">
+      <Form />
+    </div>
+  );
+}
+
+export default App;
---
+++ modulo-03/modulo-03-leccion-05/modulo-03-leccion-05-ejercicio-08-09/src/components/Form.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import InputText from './InputText';
+
+const handleFormInput = (inputName, inputValue) => {
+  console.log(inputName, inputValue);
+};
+
+const Form = () => {
+  return (
+    <form>
+      <InputText
+        id="name"
+        label="Escribe tu nombre:"
+        name="name"
+        handleInput={handleFormInput}
+      />
+      <InputText
+        id="email"
+        label="Escribe tu email:"
+        name="email"
+        handleInput={handleFormInput}
+      />
+      <InputText
+        id="ciudad"
+        label="Escribe tu ciudad:"
+        name="ciudad"
+        handleInput={handleFormInput}
+      />
+    </form>
+  );
+};
+
+export default Form;
